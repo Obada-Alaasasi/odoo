5dccd6fb4ed347675ac5f25b94a30561e0f76a46
Obada <obada.aasasi@hotmail.com>
2022-10-26 19:45:16 +0300

    added the estate property xml in manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,7 @@
     'name':'estate',
     'application': True,
     'data':[
-        './security/ir.model.access.csv'
+        './security/ir.model.access.csv',
+        './views/estate_property_views.xml'
     ]
 }
