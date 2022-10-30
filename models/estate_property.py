ad0a47f76ea007dba0297bbd4b8a5f7b6ff670ca
Obada <obada.aasasi@hotmail.com>
2022-10-30 17:41:13 +0300

    completed the property addition menu and action

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,4 +1,6 @@
 from odoo import fields, models
+from datetime import datetime, timedelta
+from dateutil.relativedelta import *
 
 #the model will be mapped to a PostgreSQL table in the odoo database through ORM
 class estate_property(models.Model):
@@ -10,14 +12,20 @@ class estate_property(models.Model):
     name = fields.Char(required = True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy = False, default = (datetime.now() + relativedelta(months =+ 3)).strftime("%Y-%m-%d") )
     expected_price = fields.Float(required = True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly = True, copy =False)
+    bedrooms = fields.Integer(default = 2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(selection = [('North', 'North'), ('South', 'South'), ('East', 'East'), ('West', 'West')])
+    state = fields.Selection(required = True, copy = False, selection=[('New', 'New'),\
+        ('Offer Received', 'Offer Received'),\
+            ('Offer Accepted', 'Offer Accepted'),\
+                ('Sold', 'Sold'),\
+                    ('Canceled', 'Canceled')], default = 'New')
+    active = fields.Boolean(default = True)
 
