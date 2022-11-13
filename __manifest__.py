bf965e977fafaee57407ec15fc662a592126477b
Obada <obada.aasasi@hotmail.com>
2022-11-13 19:37:50 +0300

    added all dependencies

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,8 @@
     'data':[
         './security/ir.model.access.csv',
         './views/estate_property_views.xml',
-        './views/estate_type_views.xml',
+        './views/property_type_views.xml',
+        './views/property_tag_views.xml',
         './views/estate_menus.xml'
     ]
 }
--- models/estate_property_tags.py
+++ models/estate_property_tags.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
-class estate_property_tags(models.Model):
-    _name="estate.property.tags"
-    _description="property tags"
+class estate_property_tag(models.Model):
+    _name="estate.property.tag"
+    _description="property tag"
 
     name = fields.Char(required=True)
