93f82164e0c2016c356416563c693a1b3dbc6d80
Obada <obada.aasasi@hotmail.com>
2022-11-24 12:32:34 +0300

    added sql constraints (unique types)

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -3,6 +3,9 @@ from odoo import models, fields
 class estate_property_type(models.Model):
     _name= "estate.property.type"
     _description= "property type"
+    _sql_constraints = [
+        ('type_unique', 'UNIQUE(name)', 'A property type must be unique')
+    ]
 
     #define model fields (columns in the SQL table)
     name= fields.Char(required = True)
