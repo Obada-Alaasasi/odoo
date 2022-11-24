ffce14f33a9cd5d4220684227d8ccba2055e6453
Obada <obada.aasasi@hotmail.com>
2022-11-24 12:32:55 +0300

    added sql constraints (unique tags)

--- models/estate_property_tags.py
+++ models/estate_property_tags.py
@@ -3,5 +3,8 @@ from odoo import models, fields
 class estate_property_tag(models.Model):
     _name="estate.property.tag"
     _description="property tag"
+    _sql_constraints = [
+        ('tag_unique', 'UNIQUE(name)','A property tag must be unique!')
+    ]
 
     name = fields.Char(required=True)
