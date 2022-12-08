d9fae3dd73b5e38e27834a5d3459cf736485c4b0
Obada <obada.aasasi@hotmail.com>
2022-12-08 15:16:18 +0300

    added the groups Agent and Manager to estate module

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,10 @@
     'application': True,
     'installable':True,
     'depends':['base'],
+    'category':'Real Estate/Brokerage',
     'data':[
         './security/ir.model.access.csv',
+        './security/security.xml',
         './views/estate_property_views.xml',
         './views/property_type_views.xml',
         './views/property_tag_views.xml',
