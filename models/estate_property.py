190f18e4b283f9f792b02776dbbafb03590dc90a
Obada <obada.aasasi@hotmail.com>
2022-12-10 12:43:41 +0300

    users can now issue invoices with elevated access rights

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,15 +1,25 @@
 from odoo import models, fields, api, Command
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, AccessError
 
 class inh_estate_property(models.Model):
     _inherit = 'estate.property'
 
     def property_sold(self):
+        
+        # print('reached'.center(100, '='))
+
+        # check acess priveleges
+        self.env['estate.property'].check_access_rights('write', True)
+        self.env['estate.property'].check_access_rule('write')
+
+        # print('reached'.center(100, '=')) #accessed
+
         #create the invoice lines
-        self.env['account.move'].create({
+        #NOTE: two sudo() methods here are requried to access two different models (account.move, account.journal)
+        self.env['account.move'].sudo().create({
             'partner_id':self.partner_id.id,
             'move_type': 'out_invoice',
-            'journal_id': self.env['account.journal'].search([('name','=','Customer Invoices')]).id,
+            'journal_id': self.env['account.journal'].sudo().search([('name','=','Customer Invoices')]).id,
             'line_ids':[
                 # invoice line #1: 6% selling price 
                 Command.create({
