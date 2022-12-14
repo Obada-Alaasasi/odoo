b124295593d038ae78900d4d4e363ebb25509144
Obada <obada.aasasi@hotmail.com>
2022-12-14 18:55:18 +0300

    added demo.xml as a data file because of an issue where demo files are skipped during odoo instance init

--- __manifest__.py
+++ __manifest__.py
@@ -13,9 +13,7 @@
         './views/property_offer_views.xml',
         './views/estate_menus.xml',
         './views/res_users_inherited_view.xml',
-        './data/estate.property.type.csv'
-    ], 
-    'demo':[
-        './demo/demo_property.xml'
+        './data/estate.property.type.csv',
+        './demo/demo.xml'
     ]
 }
